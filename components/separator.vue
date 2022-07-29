b74bef7d6ca5426f0f0f1c4378c47b1b6556e30f
Blade Barringer <blade@crookedneighbor.com>
2022-07-29 12:44:08 -0500

    feat: create separator component

---
+++ components/separator.vue
@@ -0,0 +1,9 @@
+<template>
+  <BackgroundWash color="black" class="separator"></BackgroundWash>
+</template>
+
+<style scoped>
+.separator {
+  @apply h-12 border-double border-white border-4 border-x-0;
+}
+</style>
