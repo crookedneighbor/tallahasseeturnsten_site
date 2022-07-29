4a410209a1e42387e31811f9907720a8ba00b1f3
Blade Barringer <blade@crookedneighbor.com>
2022-07-29 12:45:15 -0500

    feat: create background wash component

---
+++ components/background-wash.vue
@@ -0,0 +1,38 @@
+<template>
+  <div :class="'wash-' + color"><slot /></div>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  color: {
+    type: String,
+    default: "red",
+  },
+});
+</script>
+
+<style scoped>
+.wash-red {
+  background-image: url("/washes/redwash.jpg");
+}
+
+.wash-blue {
+  background-image: url("/washes/bluewash.jpg");
+}
+
+.wash-yellow {
+  background-image: url("/washes/yellowwash.jpg");
+}
+
+.wash-purple {
+  background-image: url("/washes/purplewash.jpg");
+}
+
+.wash-white {
+  background-image: url("/washes/whitewash.jpg");
+}
+
+.wash-black {
+  background-image: url("/washes/blackwash.jpg");
+}
+</style>
